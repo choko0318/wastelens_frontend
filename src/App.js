c0415753be2699be49e0a494efff00759a49095e
choko0318 <sdhspring@gmail.com>
2024-04-16 13:32:48 +0900

    Create React App
    Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./wastelenslogo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
